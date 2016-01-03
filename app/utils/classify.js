e84652b045e7ad94a42fe72ee8b366bf02d73608
jamesstidard <jamesstidard@gmail.com>
2016-01-03 01:28:44 +0000

    prettify classify

--- app/utils/classify.js
+++ app/utils/classify.js
@@ -1,9 +1,9 @@
-import User     from 'app/model/user'
-import Event    from 'app/model/event'
-import Invite   from 'app/model/invite'
-import Booking  from 'app/model/booking'
-import Resource from 'app/model/resource'
 import Calendar from 'app/model/calendar'
+import Resource from 'app/model/resource'
+import Booking  from 'app/model/booking'
+import Invite   from 'app/model/invite'
+import Event    from 'app/model/event'
+import User     from 'app/model/user'
 
 
 export default function classify(json) {
@@ -12,12 +12,12 @@ export default function classify(json) {
 
     switch ( type.toLowerCase() )
     {
-        case     'user': return new User(json)
-        case    'event': return new Event(json)
-        case   'invite': return new Invite(json)
-        case  'booking': return new Booking(json)
-        case 'resource': return new Resource(json)
         case 'calendar': return new Calendar(json)
+        case 'resource': return new Resource(json)
+        case  'booking': return new Booking(json)
+        case   'invite': return new Invite(json)
+        case    'event': return new Event(json)
+        case     'user': return new User(json)
 
         default return json
     }
