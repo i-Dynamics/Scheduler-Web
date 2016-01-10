96ab232cb1ef013b54b1eb538acde760e7d091d6
jamesstidard <jamesstidard@gmail.com>
2016-01-10 20:38:24 +0000

    only bind calendar panel to window on debug

--- app/components/calendar-panel/calendar.js
+++ app/components/calendar-panel/calendar.js
@@ -2,7 +2,10 @@ import './calendar.css!'
 
 import tmpl from './calendar.html!text'
 
+import {debug} from 'consts'
+
 import Vue from 'vue'
+
 import moment from 'moment'
 import $ from 'jquery'
 import fullCalendar from 'fullcalendar'
@@ -42,7 +45,7 @@ export default Vue.extend({
         }
     },
     ready() {
-        window.calendar_panel = this;
+        if (debug) window.calendar_panel = this;
 
         this.$root.control.get_bookings(this.calendar)
 
