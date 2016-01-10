83e97d12af25d0077c3cd006c1b0b2662c8bdbba
Oliver Bunyan <Dashb@me.com>
2016-01-10 17:50:48 +0000

    Fixed panel sizes

--- consts.js
+++ consts.js
@@ -1,2 +1,3 @@
-export const debug = true
-export const ws_url = 'ws://localhost:8888/v1/websocket'
+export const debug = false
+export const ws_url = 'wss://a2z-scheduler-dev.herokuapp.com/v1/websocket'
+
